44c58b5281d5ade938a7b2e719a84a66a310ea37
mandmCn <mandm_cn@hotmail.com>
2014-11-28 21:53:15 +0800

    optimize logic for pyPong

--- examples/Listing_19-5.py
+++ examples/Listing_19-5.py
@@ -14,7 +14,7 @@ class MyBallClass(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
         self.speed = speed
-        
+
     def move(self):
         global points, score_text
         self.rect = self.rect.move(self.speed)
@@ -23,7 +23,7 @@ class MyBallClass(pygame.sprite.Sprite):
             self.speed[0] = -self.speed[0]
             if self.rect.top < screen.get_height():
                 hit_wall.play()
-        
+
         # bounce off the top of the window
         if self.rect.top <= 0 :
             self.speed[1] = -self.speed[1]
@@ -39,6 +39,7 @@ class MyPaddleClass(pygame.sprite.Sprite):
         self.image    = image_surface.convert()
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("bg_music.mp3")
@@ -57,7 +58,7 @@ get_point.set_volume(0.2)
 bye = pygame.mixer.Sound("game_over.wav")
 bye.set_volume(0.6)
 screen = pygame.display.set_mode([640,480])
-clock = pygame.time.Clock()                                   
+clock = pygame.time.Clock()
 myBall = MyBallClass('wackyball.bmp', [12,6], [50, 50])
 ballGroup = pygame.sprite.Group(myBall)
 paddle = MyPaddleClass([270, 400])
@@ -74,18 +75,20 @@ while 1:
     clock.tick(30)
     screen.fill([255, 255, 255])
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEMOTION:
             paddle.rect.centerx = event.pos[0]
-    
-    if pygame.sprite.spritecollide(paddle, ballGroup, False):
-        hit.play()
-        myBall.speed[1] = -myBall.speed[1]
-        
-    myBall.move()
-   
-    if not done:
+
+    if done:
+        pass
+    else:
+        if pygame.sprite.spritecollide(paddle, ballGroup, False):
+            hit.play()
+            myBall.speed[1] = -myBall.speed[1]
+
+        myBall.move()
+
         screen.blit(myBall.image, myBall.rect)
         screen.blit(paddle.image, paddle.rect)
         screen.blit(score_text, textpos)
@@ -93,33 +96,31 @@ while 1:
             width = screen.get_width()
             screen.blit(myBall.image, [width - 40 * i, 20])
         pygame.display.flip()
-    
-    if myBall.rect.top >= screen.get_rect().bottom:
-        if not done:
+
+        if myBall.rect.top >= screen.get_rect().bottom:
             splat.play()
-        # lose a life if the ball hits the bottom
-        lives = lives - 1
-        if lives <= 0:
-            if not done:
+            # lose a life if the ball hits the bottom
+            lives = lives - 1
+            if lives <= 0:
                 pygame.time.delay(1000)
-                bye.play()  
-            final_text1 = "Game Over"
-            final_text2 = "Your final score is:  " + str(points)
-            ft1_font = pygame.font.Font(None, 70)
-            ft1_surf = font.render(final_text1, 1, (0, 0, 0))
-            ft2_font = pygame.font.Font(None, 50)
-            ft2_surf = font.render(final_text2, 1, (0, 0, 0))
-            screen.blit(ft1_surf, [screen.get_width()/2 - \
-                        ft1_surf.get_width()/2, 100])
-            screen.blit(ft2_surf, [screen.get_width()/2 - \
-                        ft2_surf.get_width()/2, 200])
-            pygame.display.flip()
-            done = True
-            pygame.mixer.music.fadeout(2000)
-        else:  #wait 2 seconds, then start the next ball
-            pygame.time.delay(1000)            
-            new_life.play()
-            myBall.rect.topleft = [50, 50]
-            screen.blit(myBall.image, myBall.rect)
-            pygame.display.flip()
-            pygame.time.delay(1000)
+                bye.play()
+                final_text1 = "Game Over"
+                final_text2 = "Your final score is:  " + str(points)
+                ft1_font = pygame.font.Font(None, 70)
+                ft1_surf = font.render(final_text1, 1, (0, 0, 0))
+                ft2_font = pygame.font.Font(None, 50)
+                ft2_surf = font.render(final_text2, 1, (0, 0, 0))
+                screen.blit(ft1_surf, [screen.get_width()/2 - \
+                            ft1_surf.get_width()/2, 100])
+                screen.blit(ft2_surf, [screen.get_width()/2 - \
+                            ft2_surf.get_width()/2, 200])
+                pygame.display.flip()
+                done = True
+                pygame.mixer.music.fadeout(2000)
+            else:  #wait 2 seconds, then start the next ball
+                pygame.time.delay(1000)
+                new_life.play()
+                myBall.rect.topleft = [50, 50]
+                screen.blit(myBall.image, myBall.rect)
+                pygame.display.flip()
+                pygame.time.delay(1000)
